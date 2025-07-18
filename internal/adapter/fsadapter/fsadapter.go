2812611904d96cfad45247fd2267ad9fbd1cf34a
Eugene Sokolov <jgivc@ya.ru>
2025-07-18 11:16:59 +0300

    save

--- internal/adapter/fsadapter/fsadapter.go
+++ internal/adapter/fsadapter/fsadapter.go
@@ -162,7 +162,6 @@ func (a *fsAdapter) readFiles(folderPath string) ([]*entity.File, error) {
 			}
 
 			fDesc.URL = filepath.Join("/", filepath.Base(a.rootDor), strings.Replace(fDesc.SourcePath, a.rootDor, "/", 1))
-			fmt.Println("AAA", fDesc)
 
 			if _, exists := a.skipFiles[fDesc.Name]; exists {
 				a.log.Info("Skip file", slog.String("path", fDesc.SourcePath))
